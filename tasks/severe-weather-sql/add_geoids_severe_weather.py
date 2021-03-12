c45706cf93f5d8eea2a5a63868a923ad4f946a68
nayanika2304 <nayanikabhargava1993@gmail.com>
2021-03-12 13:38:38 -0500

    changes in QA for populating geoids

--- tasks/severe-weather-sql/add_geoids_severe_weather.py
+++ tasks/severe-weather-sql/add_geoids_severe_weather.py
@@ -22,58 +22,40 @@ def get_state_fips_code(cursor):
     print('FETCHED STATE FIPS')
     return fips_data
 
-
-def check_for_state_fips(cursor):
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH STATE FIPS ...')
-    fips = get_state_fips_code(cursor)
-    for fip in fips:
-        sql = """
+def check_for_state_fips_except_outlying_areas(cursor,conn,fip):
+    sql = """
         UPDATE severe_weather.details
         SET geoid = state_fips::TEXT
-        WHERE geoid IS NULL
-        AND state_fips = """ + fip["fips"] + """
+        WHERE state_fips = """+str(fip)+"""
         """
-        cursor.execute(sql)
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH STATE FIPS DONE')
 
+    cursor.execute(sql)
+    conn.commit()
 
-def check_for_county_fips(cursor):
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS NOT Z ...')
-    fips = get_state_fips_code(cursor)
-    for fip in fips:
-        sql = """
+def check_for_state_fips_outlying_areas(cursor,conn,outlying_areas):
+    sql = """
+            UPDATE severe_weather.details
+            SET geoid = """+outlying_areas['actual_fips']+"""
+            WHERE state_fips = """+outlying_areas['table_fips']+"""
+            """
+
+    cursor.execute(sql)
+    conn.commit()
+
+def check_for_county_fips_except_outlying_areas(cursor,conn,fip):
+    sql = """
         UPDATE severe_weather.details
         SET geoid = LPAD(state_fips::TEXT, 2, '0') || LPAD(cz_fips::TEXT, 3, '0')
-        WHERE geoid IS NULL
-        AND state_fips = """ + fip["fips"] + """
+        WHERE state_fips =  """+str(fip)+"""
         AND cz_type IN ('C')
         """
-        cursor.execute(sql)
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS NOT Z DONE')
-
-def check_for_weather_zone(cursor):
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS Z ...')
-    sql = """
-        UPDATE severe_weather.details as a
-        SET geoid = b.fips
-        FROM severe_weather.zone_to_county as b 
-        WHERE lower(a.cz_name) = lower(b.county)
-        AND geoid  is null 
-        and cz_type IN ('C','Z')
-    """
     cursor.execute(sql)
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS Z DONE')
+    conn.commit()
 
-'''
-rows where they are outlying areas but the state fips are incorrect in Storm events 
-'''
-def check_for_weather_zone_outlying_areas(cursor):
-    print('POPULATING OUTLYING AREAS GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS C OR  Z ...')
-
+def check_for_county_fips_outlying_areas(cursor,conn,outlying_areas):
     sql = """
         UPDATE severe_weather.details as a
         SET geoid = (
-			
 					CASE 
 						WHEN state_fips = '97' THEN LPAD(60::TEXT, 2, '0') || LPAD(cz_fips::TEXT, 3, '0')
 						WHEN state_fips = '98' THEN LPAD(66::TEXT, 2, '0') || LPAD(cz_fips::TEXT, 3, '0')
@@ -81,26 +63,51 @@ def check_for_weather_zone_outlying_areas(cursor):
 						WHEN state_fips = '99' THEN LPAD(72::TEXT, 2, '0') || LPAD(cz_fips::TEXT, 3, '0')
 					END 
 					)
-        WHERE geoid is null 
-        and cz_type IN ('C','Z')
+        WHERE state_fips = """+outlying_areas['table_fips']+"""
+        AND cz_type IN ('C')
     """
     cursor.execute(sql)
-    print('POPULATING OUTLYING AREAS GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS C OR  Z DONE')
+    conn.commit()
 
-def add_cousubs(cursor):
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUSUBS ...')
-    fips = ["01", "02", "04", "05", "06", "08", "09", "10", "11", "12", "13", "15", "16", "17", "18", "19", "20", "21",
-            "22", "23", "24",
-            "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42",
-            "44", "45", "46",
-            "47", "48", "49", "50", "51", "53", "54", "55", "56","60","66","69","72","78"]
+def check_for_weather_zone_except_outlying_areas(cursor,conn,fip):
 
-    print(len(fips))
-    for fip in fips:
-        print(fip)
-        geo_table_name = 'geo.tl_2017_' + fip + '_cousub'
-        sql = """
-        
+    sql = """
+        UPDATE severe_weather.details as a
+        SET geoid = b.fips
+        FROM severe_weather.zone_to_county as b 
+        WHERE lower(a.cz_name) = lower(b.county)
+        AND a.state_fips::TEXT= substring(b.fips::TEXT,1,2) 
+        and a.state_fips = """ + str(fip) + """
+        and cz_type IN ('Z')
+    """
+    cursor.execute(sql)
+    conn.commit()
+
+def check_for_weather_zone_outlying_areas(cursor,conn,outlying_areas):
+
+    sql = """
+        UPDATE severe_weather.details as a
+        SET geoid = b.fips
+        FROM severe_weather.zone_to_county as b 
+        WHERE lower(a.cz_name) = lower(b.county)
+                AND 
+                (
+                    CASE 
+                            WHEN state_fips = '97' THEN 60::TEXT= substring(b.fips::TEXT,1,2)  
+                            WHEN state_fips = '98' THEN 66::TEXT= substring(b.fips::TEXT,1,2) 
+                            WHEN state_fips = '96' THEN 78::TEXT= substring(b.fips::TEXT,1,2)
+                            WHEN state_fips = '99' THEN 72::TEXT= substring(b.fips::TEXT,1,2)
+                    END 
+                )
+        and state_fips = """+outlying_areas['table_fips']+"""
+        and cz_type IN ('Z')
+    """
+    cursor.execute(sql)
+    conn.commit()
+
+def add_cousubs_except_outlying_areas(cursor,conn,fip):
+    geo_table_name = 'geo.tl_2017_' + fip + '_cousub'
+    sql = """
         UPDATE severe_weather.details
         SET cousub_geoid = (
             SELECT geotl.geoid
@@ -108,23 +115,33 @@ def add_cousubs(cursor):
             WHERE ST_Contains(ST_TRANSFORM(geotl.geom,4326),
 							  ST_TRANSFORM(severe_weather.details.begin_coords_geom,4326))
         )
-        WHERE cousub_geoid IS NULL
-        AND (state_fips = """ + fip + """)
+    
+        WHERE (state_fips = """+str(fip)+""")
         AND begin_coords_geom IS NOT NULL;
         """
-        cursor.execute(sql)
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUSUBS DONE')
+    cursor.execute(sql)
+    conn.commit()
 
-def add_tracts(cursor):
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH TRACTS ...')
-    fips = ["01", "02", "04", "05", "06", "08", "09", "10", "11", "12", "13", "15", "16", "17", "18", "19", "20", "21",
-            "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
-            "40", "41", "42", "44", "45", "46", "47", "48", "49", "50", "51", "53", "54", "55", "56","60","66","69","72","78"]
-    print(len(fips))
-    for fip in fips:
-        print(fip)
-        geo_table_name = 'geo.tl_2017_' + fip + '_tract'
-        sql = """
+def add_cousubs_outlying_areas(cursor,conn,outlying_areas):
+    geo_table_name = 'geo.tl_2017_' + outlying_areas['actual_fips'] + '_cousub'
+    sql = """
+        UPDATE severe_weather.details
+        SET cousub_geoid = (
+            SELECT geotl.geoid
+            FROM """ + geo_table_name + """ AS geotl
+            WHERE ST_Contains(ST_TRANSFORM(geotl.geom,4326),
+							  ST_TRANSFORM(severe_weather.details.begin_coords_geom,4326))
+        )
+    
+        WHERE (state_fips = """ + outlying_areas['table_fips'] + """)
+        AND begin_coords_geom IS NOT NULL;
+        """
+    cursor.execute(sql)
+    conn.commit()
+
+def add_tracts_except_outlying_areas(cursor,conn,fip):
+    geo_table_name = 'geo.tl_2017_' + fip + '_tract'
+    sql = """
         UPDATE severe_weather.details
         SET geoid= (
             SELECT geotl.geoid
@@ -132,12 +149,29 @@ def add_tracts(cursor):
             WHERE ST_Contains(ST_TRANSFORM(geotl.geom,4326),
 							  ST_TRANSFORM(severe_weather.details.begin_coords_geom,4326))
         )
-        WHERE geoid IS NULL
-        AND (state_fips = """ + fip + """)
+        
+        WHERE (state_fips = """ + str(fip) + """)
         AND begin_coords_geom IS NOT NULL;
         """
-        cursor.execute(sql)
-    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH TRACTS DONE')
+    cursor.execute(sql)
+    conn.commit()
+
+def add_tracts_outlying_areas(cursor,conn,outlying_areas):
+    geo_table_name = 'geo.tl_2017_' + outlying_areas['actual_fips'] + '_tract'
+    sql = """
+        UPDATE severe_weather.details
+        SET geoid= (
+            SELECT geotl.geoid
+            FROM """ + geo_table_name + """ AS geotl    
+            WHERE ST_Contains(ST_TRANSFORM(geotl.geom,4326),
+							  ST_TRANSFORM(severe_weather.details.begin_coords_geom,4326))
+        )
+        
+        WHERE (state_fips = """ + outlying_areas['table_fips'] + """)
+        AND begin_coords_geom IS NOT NULL;
+        """
+    cursor.execute(sql)
+    conn.commit()
 
 def main():
     with open('../../config/postgres.env') as f:
@@ -153,12 +187,62 @@ def main():
                             password=environ.get('PGPASSWORD'))
     cursor = conn.cursor()
 
-    check_for_state_fips(cursor)
-    check_for_county_fips(cursor)
-    check_for_weather_zone(cursor)
-    check_for_weather_zone_outlying_areas(cursor)
-    add_cousubs(cursor)
-    add_tracts(cursor)
+    fips_data = get_state_fips_code(cursor)
+    outlying_fips = ['60','66','78','72']
+    outlying_areas = [{'table_fips':'97','actual_fips':'60'},{'table_fips':'98','actual_fips':'66'},{'table_fips':'96','actual_fips':'78'},{'table_fips':'99','actual_fips':'72'}]
+    fips_data = filter(lambda x: x['fips'] not in outlying_fips, fips_data)
+
+
+    for fip in list(fips_data):
+        print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH STATE FIPS ...',fip)
+        check_for_state_fips_except_outlying_areas(cursor,conn,fip['fips'])
+    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH STATE FIPS DONE')
+
+    for outlying in outlying_areas:
+        print('POPULATING GEOID FOR OUTLYING AREAS IN SEVERE WEATHER TABLE WITH STATE FIPS IN OUTLYING AREAS ...',outlying)
+        check_for_state_fips_outlying_areas(cursor,conn,outlying)
+    print('POPULATING GEOID FOR OUTLYING AREAS IN SEVERE WEATHER TABLE WITH STATE FIPS DONE')
+
+    for fip in fips_data:
+        print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS C ...',fip)
+        check_for_county_fips_except_outlying_areas(cursor,conn,fip['fips'])
+    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS C DONE')
+
+    for outlying in outlying_areas:
+        print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS C ...',outlying)
+        check_for_county_fips_outlying_areas(cursor,conn,outlying)
+    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS C DONE')
+
+    for fip in fips_data:
+        print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS Z ...',fip)
+        check_for_weather_zone_except_outlying_areas(cursor,conn,fip['fips'])
+    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS Z DONE')
+
+    for outlying in outlying_areas:
+        print('POPULATING GEOID FOR OUTLYING AREAS IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS Z ...',outlying)
+        check_for_weather_zone_outlying_areas(cursor,conn,outlying)
+    print('POPULATING GEOID FOR OUTLYING AREAS IN SEVERE WEATHER TABLE WITH COUNTY GEOIDS WHERE CZ_TYPE IS Z DONE')
+
+
+    for fip in fips_data:
+        print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUSUBS ...',fip)
+        add_cousubs_except_outlying_areas(cursor,conn,fip['fips'])
+    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH COUSUBS DONE')
+
+    for outlying in outlying_areas:
+        print('POPULATING GEOID FOR OUTLYING AREAS IN SEVERE WEATHER TABLE WITH COUSUBS ...',outlying)
+        add_cousubs_outlying_areas(cursor,conn,outlying)
+    print('POPULATING GEOID FOR OUTLYING AREAS IN SEVERE WEATHER TABLE WITH COUSUBS DONE')
+
+    for fip in fips_data:
+        print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH TRACTS ...',fip)
+        add_tracts_except_outlying_areas(cursor,conn,fip['fips'])
+    print('POPULATING GEOID IN SEVERE WEATHER TABLE WITH TRACTS DONE')
+
+    for outlying in outlying_areas:
+        print('POPULATING GEOID FOR OUTLYING AREAS IN SEVERE WEATHER TABLE WITH TRACTS ...',outlying)
+        add_tracts_outlying_areas(cursor,conn,outlying)
+    print('POPULATING GEOID FOR OUTLYING AREAS IN SEVERE WEATHER TABLE WITH TRACTS DONE')
 
 
     conn.commit()
